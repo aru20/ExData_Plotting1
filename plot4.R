4fb0044a2c08dd635fed215e66f813089cbf44ee
AA <arundhati.panigrahi@gmail.com>
2021-03-18 14:47:28 -0500

    Changed the graph

--- plot4.R
+++ plot4.R
@@ -36,6 +36,6 @@ plot(DataTime, req.data$Sub_metering_1,xlab= "",ylab="Energy sub Metering", type
 lines(DataTime,req.data$Sub_metering_1,type = "l",col="black")
 lines(DataTime,req.data$Sub_metering_2,type = "l",col="red")
 lines(DataTime,req.data$Sub_metering_3,type = "l",col="blue")
-legend("topright",legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lty=1,lwd=2.5,col=c("black","red","blue"))
+legend("topright",legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=1,col=c("black","red","blue"),bty = "n")
 plot(DataTime,req.data$Global_active_power,xlab="datetime",ylab="Global_Active_Power",type="l")
 dev.off()
